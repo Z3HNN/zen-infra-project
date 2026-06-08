a9fa17149df8d07ac046fa872e3ab4a5704a6717
Z3HNN <z3hndolphin@gmail.com>
2026-06-08 00:39:52 -0400

    fix instance type to t3.micro

--- ec2/main.tf
+++ ec2/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "ec2" {
 
 resource "aws_instance" "main" {
     ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     subnet_id = var.subnet_id
     vpc_security_group_ids = [aws_security_group.ec2.id]
     associate_public_ip_address = true
